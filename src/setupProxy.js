8c0a25c91ae1f50a22257e3692db012a708be06e
Alex Opryshko <alexopryshko@yandex.ru>
2019-07-27 15:49:07 +0300

    media proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,4 +2,5 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(proxy('/api/', { target: 'http://127.0.0.1:8000', "changeOrigin": true }));
+  app.use(proxy('/media/', { target: 'http://127.0.0.1:8000', "changeOrigin": true }));
 };

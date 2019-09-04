5f2f4cf6bb2e028c5469041bbaa3bd404bc17061
iakrygin <krygin.ia@gmail.com>
2019-09-04 16:29:54 +0300

    update vk deps

--- src/site/AuthWrapper.js
+++ src/site/AuthWrapper.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Base from 'shared/components/Base';
 import { connect as reduxConnect } from 'react-redux';
-import connect from '@vkontakte/vkui-connect';
+import connect from '@vkontakte/vk-connect';
 import { bindActionCreators } from 'redux';
 import { actions } from 'store/entities/user';
 
